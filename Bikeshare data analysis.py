4afdb7fc7f97e583f05ba1b5c58f7c6c41d6cfbc
rspeegs <ryan.speegle1@gmail.com>
2026-08-20 16:11:32 -0400

    Updated bikeshare analysis
    Transitioned the basic analysis into functions for reusability over many data frames at once

--- Bikeshare data analysis.py
+++ Bikeshare data analysis.py
@@ -6,52 +6,55 @@ import math
 #___________________________________________
 #                       DATA
 
-df = pd.read_csv("202507-divvy-tripdata.csv") # July 2025
-#df = pd.read_csv("202508-divvy-tripdata.csv") # August 2025
-#df = pd.read_csv("202509-divvy-tripdata.csv") # September 2025
-#df = pd.read_csv("202510-divvy-tripdata.csv") # October 2025
-#df = pd.read_csv("202511-divvy-tripdata.csv") # November 2025
-#df = pd.read_csv("202512-divvy-tripdata.csv") # December 2025
-#df = pd.read_csv("202601-divvy-tripdata.csv") # January 2026
-#df = pd.read_csv("202602-divvy-tripdata.csv") # February 2026
-#df = pd.read_csv("202603-divvy-tripdata.csv") # March 2026
-#df = pd.read_csv("202604-divvy-tripdata.csv") # April 2026
-#df = pd.read_csv("202605-divvy-tripdata.csv") # May 2026
-#df = pd.read_csv("202606-divvy-tripdata.csv") # June 2026
-#df = pd.read_csv("202607-divvy-tripdata.csv") # July 2026
+df_2025_07 = pd.read_csv("202507-divvy-tripdata.csv") # July 2025
+df_2025_08 = pd.read_csv("202508-divvy-tripdata.csv") # August 2025
+df_2025_09 = pd.read_csv("202509-divvy-tripdata.csv") # September 2025
+df_2025_10 = pd.read_csv("202510-divvy-tripdata.csv") # October 2025
+df_2025_11 = pd.read_csv("202511-divvy-tripdata.csv") # November 2025
+df_2025_12 = pd.read_csv("202512-divvy-tripdata.csv") # December 2025
+df_2026_01 = pd.read_csv("202601-divvy-tripdata.csv") # January 2026
+df_2026_02 = pd.read_csv("202602-divvy-tripdata.csv") # February 2026
+df_2026_03 = pd.read_csv("202603-divvy-tripdata.csv") # March 2026
+df_2026_04 = pd.read_csv("202604-divvy-tripdata.csv") # April 2026
+df_2026_05 = pd.read_csv("202605-divvy-tripdata.csv") # May 2026
+df_2026_06 = pd.read_csv("202606-divvy-tripdata.csv") # June 2026
+df_2026_07 = pd.read_csv("202607-divvy-tripdata.csv") # July 2026
 
 #_____________________________________________
 
 #print(set(df["rideable_type"]))
 #print(set(df["member_casual"]))
 
-total_rider_count = 0
-casual_rider_count = 0
-member_rider_count = 0
-for rider in df["member_casual"]:
-    total_rider_count += 1
-    if rider == "member":
-        member_rider_count += 1
-    if rider == "casual":
-        casual_rider_count += 1
+def find_rider_counts(dataframe):
+    total_rider_count = 0
+    casual_rider_count = 0
+    member_rider_count = 0
 
-print(f"Total # of Riders: {total_rider_count}")
-print(f"# of Member Riders: {member_rider_count}\n# of Causal Riders: {casual_rider_count}")
-print(f"Percentage of Member Riders: {(member_rider_count / total_rider_count):.2%}")
-print(f"Percentage of Casual Riders: {(casual_rider_count / total_rider_count):.2%}\n")
+    for rider in dataframe["member_casual"]:
+        if rider == "member" or rider == "casual":
+            total_rider_count += 1
+        if rider == "member":
+            member_rider_count += 1
+        if rider == "casual":
+            casual_rider_count += 1
 
-#TODO: Examine the average length (time and distance) of bike trips for both member types
+    percentage_of_member_riders = round((member_rider_count / total_rider_count) * 100, 4)
+    percentage_of_casual_riders = round((casual_rider_count / total_rider_count) * 100, 4)
 
-df["started_at"] = pd.to_datetime(df["started_at"])
-df["ended_at"] = pd.to_datetime(df["ended_at"])
-df["duration"] = df["ended_at"] - df["started_at"]
+    return [total_rider_count, member_rider_count, casual_rider_count, percentage_of_member_riders, percentage_of_casual_riders]
 
-df["start_lat"] = pd.to_numeric(df["start_lat"])
-df["end_lat"] = pd.to_numeric(df["end_lat"])
+def find_average_time_biking(dataframe):
+    dataframe["started_at"] = pd.to_datetime(dataframe["started_at"])
+    dataframe["ended_at"] = pd.to_datetime(dataframe["ended_at"])
+    dataframe["duration"] = dataframe["ended_at"] - dataframe["started_at"]
 
-df["start_lng"] = pd.to_numeric(df["start_lng"])
-df["end_lng"] = pd.to_numeric(df["end_lng"])
+    casual_riders = dataframe[dataframe["member_casual"] == "casual"]
+    member_riders = dataframe[dataframe["member_casual"] == "member"]
 
+    average_time_biking_casual = str(casual_riders["duration"].mean())
+    average_time_biking_member = str(member_riders["duration"].mean())
+
+    return [average_time_biking_casual, average_time_biking_member]
 
 def coordinates_to_distance(start_latitude, end_latitude, start_longitude, end_longitude, is_degrees = True, in_km = False):
 
@@ -78,20 +81,26 @@ def coordinates_to_distance(start_latitude, end_latitude, start_longitude, end_l
     else:
         return R_km * c
 
-df["distance"] = df.apply(lambda x: coordinates_to_distance(x["start_lat"], x["end_lat"], x["start_lng"], x["end_lng"]), axis=1)
+def find_average_distance_biking(dataframe):
+    dataframe["start_lat"] = pd.to_numeric(dataframe["start_lat"])
+    dataframe["end_lat"] = pd.to_numeric(dataframe["end_lat"])
 
-casual_riders = df[df["member_casual"] == "casual"]
-member_riders = df[df["member_casual"] == "member"]
+    dataframe["start_lng"] = pd.to_numeric(dataframe["start_lng"])
+    dataframe["end_lng"] = pd.to_numeric(dataframe["end_lng"])
 
-casual_rider_duration_avg = casual_riders["duration"].mean()
-member_rider_duration_avg = member_riders["duration"].mean()
+    dataframe["distance"] = dataframe.apply(lambda x: coordinates_to_distance(x["start_lat"], x["end_lat"], x["start_lng"], x["end_lng"]), axis=1)
+
+    casual_riders = dataframe[dataframe["member_casual"] == "casual"]
+    member_riders = dataframe[dataframe["member_casual"] == "member"]
+
+    average_distance_biking_casual = float(casual_riders["distance"].mean())
+    average_distance_biking_member = float(member_riders["distance"].mean())
+    
+
+    return [round(average_distance_biking_casual, 5), round(average_distance_biking_member, 5)]
 
 
-casual_rider_distance_avg = casual_riders["distance"].mean()
-member_rider_distance_avg = member_riders["distance"].mean()
-
-print(f"Average casual rider duration: {casual_rider_duration_avg}")
-print(f"Average member rider duration: {member_rider_duration_avg}\n")
-
-print(f"Average distance travelled by casual riders: {casual_rider_distance_avg:.4f}")
-print(f"Average distance travelled by member riders: {member_rider_distance_avg:.4f}")
+#testing
+print(find_rider_counts(df_2025_10))
+print(find_average_distance_biking(df_2026_01))
+print(find_average_time_biking(df_2026_05))
